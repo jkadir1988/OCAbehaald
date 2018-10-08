bf0a6d758125e0aa7a865a9187325e00e9f4a350
jkadir1988 <jusreiza@gmail.com>
2018-10-08 16:45:21 +0200

    OCA Behaald2

--- Demo.java
+++ Demo.java
@@ -1,12 +1,12 @@
 public class Demo { 
 	public static void main(String[] args) {
 		Student student1 = new Student();
-		student1.cijfer = 64;
-		student1.voornaam = "Javiel";
+		
 		
 		Student student2 = new Student();
-		student2.cijfer = 65;
-		student2.voornaam = "Jusreiza";
+		
+		student1.vullen(65, "Javiel");
+		student2.vullen(64, "Jusreiza");
 		
 		controleren(student1);
 		controleren(student2);
@@ -24,5 +24,10 @@ public class Demo {
 class Student{
 	int cijfer;
 	String voornaam;
+	
+	void vullen (int getal, String naamvoor) {
+		cijfer = getal;
+		voornaam = naamvoor;
+	}
 	
 }
